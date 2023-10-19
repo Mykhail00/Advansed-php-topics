691f3b4021f6978f044344ef86c85861f49c8a17
Mykhail00 <bazhenovm000@gmail.com>
2023-10-19 17:16:14 +0300

    3.2

---
+++ tests/Unit/RouterTest.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use App\Exceptions\RouteNotFoundException;
+use App\Router;
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    private Router $router;
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->router = new Router();
+    }
+    /**
+     * @test
+     */
+    public function it_registers_a_route(): void
+    {
+        $this->router->register('get', '/users', ['Users', 'index']);
+
+        $expected = [
+            'get' => [
+                '/users' => ['Users', 'index']
+            ]
+        ];
+
+        $this->assertSame($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function it_registers_a_get_route(): void
+    {
+
+        $this->router->get('/users', ['Users', 'index']);
+
+        $expected = [
+            'get' => [
+                '/users' => ['Users', 'index']
+            ]
+        ];
+
+        $this->assertSame($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function it_registers_a_post_route(): void
+    {
+
+        $this->router->post('/users', ['Users', 'index']);
+
+        $expected = [
+            'post' => [
+                '/users' => ['Users', 'index']
+            ]
+        ];
+
+        $this->assertSame($expected, $this->router->routes());
+    }
+
+    /** @test */
+    public function there_is_no_routes_when_router_is_created(): void
+    {
+        $router = new Router();
+
+        $this->assertEmpty($router->routes());
+    }
+
+    /**
+     * @test
+     * @dataProvider Tests\DataProviders\RouterDataProvider::routeNotFoundCases()
+     */
+    public function it_throws_route_not_found_exception(
+        string $requestUri,
+        string $requestMethod
+    ): void
+    {
+        $user = new class() {
+            public function delete(): bool
+            {
+                return true;
+            }
+        };
+
+        $this->router->post('/users', [$user::class, 'store']);
+        $this->router->get('/users', ['Users', 'index']);
+
+        $this->expectException(RouteNotFoundException::class);
+        $this->router->resolve($requestUri, $requestMethod);
+    }
+
+    /** @test */
+    public function it_resolves_a_route_from_callable(): void
+    {
+        $this->router->get('/user', fn() => [1, 2, 3]);
+        $this->assertSame(
+            [1, 2, 3],
+            $this->router->resolve('/user', 'get')
+        );
+    }
+
+    /** @test */
+    public function it_resolves_route(): void
+    {
+        $user = new class() {
+          public function index() {
+              return [1, 2, 3];
+          }
+        };
+
+        $this->router->get('/user', [$user::class, 'index']);
+
+        $this->assertSame(
+            [1, 2, 3],
+            $this->router->resolve('/user', 'get')
+        );
+    }
+}
